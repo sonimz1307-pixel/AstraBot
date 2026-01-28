2a7d577725e18608de372d10ffa6408c59509235
sonimz1307-pixel <sonimz1307@gmail.com>
2026-01-28 22:58:39 +0400

    Update db_supabase.py

--- db_supabase.py
+++ db_supabase.py
@@ -6,8 +6,6 @@ from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
-print("SUPABASE_URL set:", bool(SUPABASE_URL))
-print("SUPABASE_SERVICE_KEY set:", bool(SUPABASE_SERVICE_KEY))
 
 if not SUPABASE_URL or not SUPABASE_SERVICE_KEY:
     supabase = None
