928a88a9fdf3c827c966f613a4ea37f1a9a51adb
sonimz1307-pixel <sonimz1307@gmail.com>
2026-01-29 22:36:13 +0400

    Update test_supabase_storage.py

--- test_supabase_storage.py
+++ test_supabase_storage.py
@@ -2,7 +2,7 @@ import os
 from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 BUCKET = os.getenv("SUPABASE_BUCKET", "kling")
 
 sb = create_client(SUPABASE_URL, SUPABASE_KEY)
